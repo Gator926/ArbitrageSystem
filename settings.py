2877ef5ba564bb9d0544ffa72992f61c3ea573f8
彼岸529 <zhangpei529@vip.qq.com>
2018-03-03 10:36:55 +0800

    Initial commit

---
+++ settings.py
@@ -0,0 +1,18 @@
+settings = {
+
+    # 火币API信息
+    "ACCESS_KEY": "####",
+    "SECRET_KEY": "####",
+
+    # 阿里云短信
+    "access_key_id": "####",
+    "access_secret": "####",
+    "phone": "####",
+    "sign": "####",
+
+    # 数据库配置
+    "host": "####",
+    "user": "####",
+    "pass": "####",
+    "database_name": "####"
+}
