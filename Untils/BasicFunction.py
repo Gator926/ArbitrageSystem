5d4375d45c85ee32ea3159c5f404551e89534731
彼岸529 <zhangpei529@vip.qq.com>
2018-03-05 20:29:06 +0800

    更细基础函数模块

---
+++ Untils/BasicFunction.py
@@ -0,0 +1,71 @@
+from HuobiServices import *
+import time
+
+
+# 格式化时间
+def get_str_datetime():
+    time_local = time.localtime(time.time())
+    dt = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
+    return dt
+
+
+# 例如get_account_balance('usdt')
+def get_account_balance(currency):
+    try:
+        result = get_balance()
+    except Exception as E:
+        logger(E)
+    number = ""
+    for each in result['data']['list']:
+        if each['currency'] == currency and each['type'] == 'trade':
+            number = each['balance']
+            return number[0:number.index(".") + 5]  # 保留小数点4位
+
+
+# 全仓使用买入BTC函数
+def buy_currency(database, scli, base_currency_name, aim_currency_name):
+    try:
+        number = get_account_balance(base_currency_name)
+        result = send_order(amount=number, source='api',
+                            symbol=aim_currency_name+base_currency_name, _type='buy-market')
+    except Exception as E:
+        logger.error(E)
+
+    if result['status'] == 'ok':
+        try:
+            database.insert("insert into trade_history (order_id) values (%s)" % result['data'])
+            resp = scli.request(phone_numbers=settings['phone'],
+                                sign=settings['sign'],
+                                template_code='SMS_126355043',
+                                template_param={'time': get_str_datetime(), 'type': '买入'})
+        except Exception as E:
+            logger.error(E)
+        finally:
+            logger.info("买入成功")
+            return result
+    if result['status'] == 'error':
+        logger.error(result)
+
+
+# 全仓卖出BTC函数
+def sell_currency(database, scli, base_currency_name, aim_currency_name):
+    try:
+        number = get_account_balance(aim_currency_name)
+        result = send_order(amount=number, source='api',
+                            symbol=aim_currency_name+base_currency_name, _type='sell-market')
+    except Exception as E:
+        logger.error(E)
+    if result['status'] == 'ok':
+        try:
+            database.insert("insert into trade_history (order_id) values (%s)" % result['data'])
+            resp = scli.request(phone_numbers=settings['phone'],
+                               sign=settings['sign'],
+                               template_code='SMS_126355043',
+                               template_param={'time': get_str_datetime(), 'type': '卖出'})
+        except Exception as E:
+            logger.error(E)
+        finally:
+            logger.info("卖出成功")
+            return result
+    if result['status'] == 'error':
+        logger.error(result)
