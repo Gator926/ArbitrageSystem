8c51aa4a24bc0f0d2e1fa0ef158aedd2e2ec00f7
彼岸529 <zhangpei529@vip.qq.com>
2018-03-10 09:52:48 +0800

    修复了常用函数中的一些bug

--- Untils/BasicFunction.py
+++ Untils/BasicFunction.py
@@ -9,6 +9,18 @@ def get_str_datetime():
     dt = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
     return dt
 
+# 例如get_account_balance('usdt')
+def get_account_balance_single(currency):
+    try:
+        result = get_balance()
+    except Exception as E:
+        log(E)
+    for each in result['data']['list']:
+        if each['currency'] == currency and each['type'] == 'trade':
+            number = each['balance']
+            # data.append(number[0:number.index(".") + 5])  # 保留小数点4位
+    return number[0:number.index(".") + 5]
+
 
 # 例如get_account_balance('usdt')
 def get_account_balance(base_currency_name, aim_currency_name):
@@ -16,22 +28,21 @@ def get_account_balance(base_currency_name, aim_currency_name):
         result = get_balance()
     except Exception as E:
         log(E)
-    # number = ""
-    data = []
+    data = {}
     for each in result['data']['list']:
         if each['currency'] == base_currency_name and each['type'] == 'trade':
             number = each['balance']
-            data.append(number[0:number.index(".") + 5])  # 保留小数点4位
+            data[base_currency_name] = number[0:number.index(".") + 5]  # 保留小数点4位
         if each['currency'] == aim_currency_name and each['type'] == 'trade':
             number = each['balance']
-            data.append(number[0:number.index(".") + 5])  # 保留小数点4位
-    return Decimal(data[0]), Decimal(data[1])
+            data[aim_currency_name] = number[0:number.index(".") + 5]  # 保留小数点4位
+    return Decimal(data[1]), Decimal(data[0])
 
 
 # 全仓使用买入BTC函数
 def buy_currency(database, scli, base_currency_name, aim_currency_name):
     try:
-        number = get_account_balance(base_currency_name)
+        number = get_account_balance_single(base_currency_name)
         result = send_order(amount=number, source='api',
                             symbol=aim_currency_name+base_currency_name, _type='buy-market')
         log.info(result)
@@ -59,7 +70,7 @@ def buy_currency(database, scli, base_currency_name, aim_currency_name):
 # 全仓卖出BTC函数
 def sell_currency(database, scli, base_currency_name, aim_currency_name):
     try:
-        number = get_account_balance(aim_currency_name)
+        number = get_account_balance_single(aim_currency_name)
         result = send_order(amount=number, source='api',
                             symbol=aim_currency_name+base_currency_name, _type='sell-market')
         log.info(result)
